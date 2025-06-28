399636e4f3144849daa74a024ba94dc9e9bd258e
Made2Flex <skywalka@duck.com>
2025-06-28 02:50:49 -0400

    Update setupsnapshots.sh (#2)

--- setupsnapshots.sh
+++ setupsnapshots.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/bash
 
+# Color definitions
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+RED='\033[1;31m'
+BLUE='\033[0;34m'
+LIGHT_BLUE='\033[1;36m'
+NC='\033[0m' # No color
+
 # Function to greet the user
 greet_user() {
     local username
     username=$(whoami)
-    echo "Hello, $username-sama!"
-    sleep 1
+    echo -e "${ORANGE}Hello, $username ${NC}"
 }
 
 # Detect package manager (Arch or Debian)
 detect_package_manager() {
     if command -v pacman >/dev/null 2>&1; then
-        echo "==>> Arch-based system detected."
+        echo -e "${ORANGE}==>> Arch-based system detected.${NC}"
         PKG_MANAGER="pacman"
         AUR_HELPER="yay"
     elif command -v apt >/dev/null 2>&1; then
-        echo "==>> Debian-based system detected."
+        echo -e "${ORANGE}==>> Debian-based system detected.${NC}"
         PKG_MANAGER="apt"
     else
-        echo "==<>Unsupported system. Exiting..."
+        echo -e "${RED}==<> Unsupported system. Exiting...${NC}"
         exit 1
     fi
 }
@@ -27,19 +34,18 @@ detect_package_manager() {
 get_timeshift() {
     if [ "$PKG_MANAGER" = "pacman" ]; then
         if ! command -v timeshift >/dev/null 2>&1; then
-            echo "==>> timeshift is not installed! Installing it now..."
+            echo -e "${ORANGE}==>> timeshift is not installed! Installing it now...${NC}"
             sudo pacman -Syyu --needed --noconfirm --color=auto timeshift
         else
-            echo "==>> timeshift is already installed."
-            echo "==<> moving on..."
+            echo -e "${ORANGE}==>> timeshift is already installed.${NC}"
         fi
     elif [ "$PKG_MANAGER" = "apt" ]; then
         if ! command -v timeshift >/dev/null 2>&1; then
-            echo "==>> timeshift is not installed! Installing it now..."
+            echo -e "${ORANGE}==>> timeshift is not installed! Installing it now...${NC}"
             sudo apt update && sudo apt install -y --no-install-recommends timeshift
         else
-            echo "==>> timeshift is already installed."
-            echo "==<> moving on..."
+            echo -e "${ORANGE}==>> timeshift is already installed.${NC}"
+            echo -e "${ORANGE}==<> moving on...${NC}"
         fi
     fi
 }
@@ -48,23 +54,24 @@ get_timeshift() {
 enable_cron_serv() {
     if [ "$PKG_MANAGER" = "pacman" ]; then
         sudo systemctl enable --now cronie.service
-	echo "==>> cronie service enabled!"
+        echo -e "${GREEN}==>> cronie service enabled ✓successfully.${NC}"
     elif [ "$PKG_MANAGER" = "apt" ]; then
         sudo systemctl enable --now cron.service
-	echo "==>> cron service enabled!"
+        echo -e "${GREEN}==>> cron service enabled ✓successfully.${NC}"
     fi
 }
 
 # Install timeshift-autosnap and grub-btrfs for Arch or btrfs-progs & snapper, snapper-gui for Debian
 get_deps() {
     if [ "$PKG_MANAGER" = "pacman" ]; then
-	echo "==>> installing timeshift-autosnap from uar..."
+        echo -e "${ORANGE}==>> installing timeshift-autosnap from uar...${NC}"
         $AUR_HELPER -S --noconfirm timeshift-autosnap
+        /etc/timeshift-autosnap.conf
         sleep 1
-	echo "==>> installing grub-btrfs..."
+        echo -e "${ORANGE}==>> installing grub-btrfs...${NC}"
         sudo pacman -S --needed --noconfirm grub-btrfs
     elif [ "$PKG_MANAGER" = "apt" ]; then
-        echo "==>> Installing snapper and btrfs-progs..."
+        echo -e "${ORANGE}==>> Installing snapper and btrfs-progs...${NC}"
         sudo apt install -y --no-install-recommends snapper snapper-gui btrfs-progs
     fi
 }
@@ -92,17 +99,18 @@ edit_grub_btrfsd() {
 
 # Rebuild the grub configuration
 rebuild_grub_config() {
-	echo "==>> Reconfiguring grub.cfg"
+	echo -e "${ORANGE}==>> Reconfiguring grub.cfg.${NC}"
 	sudo grub-mkconfig -o /boot/grub/grub.cfg
 }
 
 # Enable grub-btrfsd service
 grub_btrfsd_serv() {
-	echo "==>> Enabling grub-btrfsd service..."
+	echo -e "${ORANGE}==>> Enabling grub-btrfsd service...${NC}"
     if [ "$PKG_MANAGER" = "pacman" ]; then
         sudo systemctl enable --now grub-btrfsd
+        echo -e "${GREEN}==>> grub-btrfsd service enabled ✓successfully.${NC}"
     elif [ "$PKG_MANAGER" = "apt" ]; then
-        echo "No additional services to enable for snapper."
+        echo -e "${RED}==<> No additional services to enable for snapper.${NC}"
     fi
 }
 
@@ -116,4 +124,4 @@ edit_grub_btrfsd
 rebuild_grub_config
 grub_btrfsd_serv
 
-echo "==>> BTRFS snapshot setup completed!"
+echo -e "${YELLOW}==>> BTRFS snapshot setup completed ✓successfully.${NC}"
